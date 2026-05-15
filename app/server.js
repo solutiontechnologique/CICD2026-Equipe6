a326f64a84631bba2bd40910e2eba5484972c03e
stessy12 <6249830@cegepmontpetit.ca>
2026-05-15 00:49:50 -0400

    Test test

--- app/server.js
+++ app/server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 const ENV = process.env.APP_ENV || 'dev';
 
 const messages = {
-  dev: "Bonjour au site    ",
+  dev: "Bonjour au site  cicd  ",
   qa: "Hello de l'environnement de QA",
   prod: "Hello de l'environnement de PROD",
 };
