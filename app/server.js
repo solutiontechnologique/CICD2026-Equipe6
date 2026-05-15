38fe13b9b425b206c379b10b97c6b5b3ab9585dd
stessy12 <6249830@cegepmontpetit.ca>
2026-05-15 00:44:31 -0400

    Test pipeline

--- app/server.js
+++ app/server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 const ENV = process.env.APP_ENV || 'dev';
 
 const messages = {
-  dev: "Bonjour au site   ekip ",
+  dev: "Bonjour au site    ",
   qa: "Hello de l'environnement de QA",
   prod: "Hello de l'environnement de PROD",
 };
